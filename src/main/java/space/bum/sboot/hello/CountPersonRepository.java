c08aca05e383484b17f55335bbb05d55cd215b20
osparking <jbpark03@gmail.com>
2024-07-15 21:08:42 +0900

    유도된 두 Person 삭제 메소드

--- src/main/java/space/bum/sboot/hello/CountPersonRepository.java
+++ src/main/java/space/bum/sboot/hello/CountPersonRepository.java
@@ -1,7 +1,18 @@
 package space.bum.sboot.hello;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.transaction.Transactional;
 
 public interface CountPersonRepository extends CrudRepository<Person, Long>{
   long countByName(String name);
+  
+  @Transactional
+  long deleteByName(String name);
+
+  @Transactional
+  List<Person> removeByName(String name);
 }
