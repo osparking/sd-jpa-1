6d79d75e9f2356c74083bb4d8d2931b991a4d904
osparking <jbpark03@gmail.com>
2024-07-15 16:30:36 +0900

    응용 클래스 - Person 빈 생성, 저장, 검색 실행

--- src/main/java/space/bum/sboot/SdJpa1Application.java
+++ src/main/java/space/bum/sboot/SdJpa1Application.java
@@ -1,13 +1,35 @@
 package space.bum.sboot;
 
+import java.util.NoSuchElementException;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.slf4j.Slf4j;
+import space.bum.sboot.hello.Person;
+import space.bum.sboot.hello.PersonRepository;
 
 @SpringBootApplication
+@Slf4j
 public class SdJpa1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SdJpa1Application.class, args);
 	}
 
+    @Bean
+    CommandLineRunner runner(PersonRepository repository) {
+      return args -> {
+
+        Person person = new Person();
+        person.setName("전어");
+
+        repository.save(person);
+        Person saved = repository.findById(person.getId())
+            .orElseThrow(NoSuchElementException::new);
+        log.info("DB에서 읽은 사람 이름: {}", saved.getName());
+      };
+    }
 }
