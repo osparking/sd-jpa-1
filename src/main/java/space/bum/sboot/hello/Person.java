b54a6893b27cf3d4a7d36ab70667fe3156982b05
osparking <jbpark03@gmail.com>
2024-07-15 16:23:16 +0900

    나의 세상에 유일한 개체 클래스

---
+++ src/main/java/space/bum/sboot/hello/Person.java
@@ -0,0 +1,15 @@
+package space.bum.sboot.hello;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Person {
+  @Id @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String name;
+}
