7c27a89c4fcbc9d3279065141446561921425f86
osparking <jbpark03@gmail.com>
2024-07-15 20:16:35 +0900

    유도된 count 메소드 호출 및 결과 기록

--- src/main/java/space/bum/sboot/SdJpa1Application.java
+++ src/main/java/space/bum/sboot/SdJpa1Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import lombok.extern.slf4j.Slf4j;
+import space.bum.sboot.hello.CountPersonRepository;
 import space.bum.sboot.hello.Person;
 import space.bum.sboot.hello.PersonRepository;
 
@@ -15,21 +16,25 @@ import space.bum.sboot.hello.PersonRepository;
 @Slf4j
 public class SdJpa1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SdJpa1Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SdJpa1Application.class, args);
+  }
 
-    @Bean
-    CommandLineRunner runner(PersonRepository repository) {
-      return args -> {
+  @Bean
+  CommandLineRunner runner(PersonRepository repository,
+      CountPersonRepository cpRepository) {
+    return args -> {
 
-        Person person = new Person();
-        person.setName("전어");
+      Person person = new Person();
+      person.setName("전어");
 
-        repository.save(person);
-        Person saved = repository.findById(person.getId())
-            .orElseThrow(NoSuchElementException::new);
-        log.info("DB에서 읽은 사람 이름: {}", saved.getName());
-      };
-    }
+      repository.save(person);
+      Person saved = repository.findById(person.getId())
+          .orElseThrow(NoSuchElementException::new);
+      log.info("DB에서 읽은 사람 이름: {}", saved.getName());
+      
+      long personCount = cpRepository.countByName("전어");
+      log.info("등록 사람 수: {}", personCount);
+    };
+  }
 }
