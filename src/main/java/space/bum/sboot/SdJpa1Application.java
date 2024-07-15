d53142b8b287689e8d0196e39b22dab470a513a7
osparking <jbpark03@gmail.com>
2024-07-15 21:22:00 +0900

    removeBy 메소드 호출 코드

--- src/main/java/space/bum/sboot/SdJpa1Application.java
+++ src/main/java/space/bum/sboot/SdJpa1Application.java
@@ -1,14 +1,15 @@
 package space.bum.sboot;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import space.bum.sboot.hello.CountPersonRepository;
+import space.bum.sboot.hello.Person;
 import space.bum.sboot.hello.PersonRepository;
 
 @SpringBootApplication
@@ -24,9 +25,11 @@ public class SdJpa1Application {
   CommandLineRunner runner(PersonRepository repository,
       CountPersonRepository cpRepository) {
     return args -> {
-      long delCount = cpRepository.deleteByName("전어");
-      log.info("삭제 사람 수: {}", delCount);
-      log.info("삭제 후 검색 결과: {}", repository.findById(1L).orElse(null));
+      List<Person> deletedPerson = cpRepository.removeByName("전어");
+      log.info("삭제 사람 수: {}", deletedPerson.size());
+      
+      log.info("삭제된 사람 정보 - ");
+      deletedPerson.forEach(p -> log.info(p.toString()));
     };
   }
 }
