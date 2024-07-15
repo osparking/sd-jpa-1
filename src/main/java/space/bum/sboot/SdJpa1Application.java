672bd9704f6eec2d2f0a2ca74553e2318c124fa3
osparking <jbpark03@gmail.com>
2024-07-15 15:20:41 +0900

    sd jpa 참고문서 학습 시작

---
+++ src/main/java/space/bum/sboot/SdJpa1Application.java
@@ -0,0 +1,13 @@
+package space.bum.sboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdJpa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/space/bum/sboot/SdJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package space.bum.sboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
